d07638cd098a11ed34a218b10bccf5b4f7535f0e
kushn <w3gop2p@gmail.com>
2026-06-16 20:39:21 +0300

    feat: greet multiple people and errors

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/skybytescode/hello
 
 go 1.26.4
 
-require github.com/skybytescode/greetings v0.0.0-20260613135217-cd17845ed9a1
+require github.com/skybytescode/greetings v0.0.0-20260616171834-514d1df24a69
--- hello.go
+++ hello.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"log"
 
-    "github.com/skybytescode/greetings"
+	"github.com/skybytescode/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
-    fmt.Println(message)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	 // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
+
+	
+	// If an error was returned, print it to the console and exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    // If no error was returned, print the returned map of
+    // messages to the console.
+	fmt.Println(messages)
 }
-
-
